ea79cb1e4667b751371d33ac5629955b1c11966a
suadtalafha <suadhusam@yahoo.com>
2021-09-20 23:25:44 +0300

    save

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,13 +1,20 @@
-import React from "react";
+import JSONPretty from 'react-json-pretty';
+import 'react-json-pretty/themes/monikai.css';
+import Loading from '../loading/loading';
 
 function Results(props) {
+
   return (
     <section>
-      <pre>
-        {props.data ? JSON.stringify(this.props.data, undefined, 2) : null}
-      </pre>
+      {props.data ? <div>
+        <h2>Headers</h2>
+        <JSONPretty id="json-pretty" data={props.data ? props.data.headers : null}></JSONPretty>
+        <h2>Results</h2>
+        <JSONPretty id="json-pretty" data={props.data ? props.data.data : null}></JSONPretty>
+      </div> : <Loading />}
     </section>
   );
+
 }
 
 export default Results;
